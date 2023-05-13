cdff331d888598b3974b2f09666a2355caf15264
likanu-g <1452930742@qq.com>
2023-05-13 14:26:30 +0800

    Initial commit

---
+++ readame.md
@@ -0,0 +1,7 @@
+# 项目说明
+
+## 1、运行环境
+
+jdk17，使用最新的idea或者eclipse等编辑器打开项目，由于然后引入library目录下的junit-4.12.jar以及hamcrest-core-1.3.jar(junit需要依赖此jar包不然无法运行)。
+
+然后运行`Main.java`文件。
---
+++ src/ataxx/AIPlayer.java
@@ -0,0 +1,108 @@
+package ataxx;
+
+import java.util.ArrayList;
+
+// Final Project Part A.2 Ataxx AI Player (A group project)
+
+/** A Player that computes its own moves. */
+class AIPlayer extends Player {
+
+    
+    /** A new AIPlayer for GAME that will play MYCOLOR.
+     *  SEED is used to initialize a random-number generator,
+	 *  increase the value of SEED would make the AIPlayer move automatically.
+     *  Identical seeds produce identical behaviour. */
+    AIPlayer(Game game, PieceState myColor, long seed) {
+        super(game, myColor);
+    }
+
+    @Override
+    boolean isAuto() {
+        return true;
+    }
+
+    @Override
+    String getAtaxxMove() {
+        Move move = findMove();
+        getAtaxxGame().reportMove(move, getMyState());
+        return move.toString();
+    }
+
+    /** Return a move for me from the current position, assuming there
+     *  is a move. */
+    private Move findMove() {
+        Board b = new Board(getAtaxxBoard());
+        lastFoundMove = null;
+
+        // Here we just have the simple AI to randomly move.
+        // However, it does not meet with the requirements of Part A.2.
+        // Therefore, the following codes should be modified
+        // in order to meet with the requirements of Part A.2.
+        // You can create add your own method and put your method here.
+		
+        ArrayList<Move> listOfMoves =
+                possibleMoves(b, b.nextMove());
+        int moveArrayLength = listOfMoves.size();
+        int randomIndex = (int) (Math.random() * moveArrayLength);
+        for(int i = 0; i < moveArrayLength; i++){
+            if (i == randomIndex){
+                b.createMove(listOfMoves.get(i));
+                lastFoundMove = listOfMoves.get(i);
+            }
+        }
+		
+
+
+        // Please do not change the codes below
+        if (lastFoundMove == null) {
+            lastFoundMove = Move.pass();
+        }
+        return lastFoundMove;
+    }
+
+
+    /** The move found by the last call to the findMove method above. */
+    private Move lastFoundMove;
+
+
+    /** Return all possible moves for a color.
+     * @param board the current board.
+     * @param myColor the specified color.
+     * @return an ArrayList of all possible moves for the specified color. */
+    private ArrayList<Move> possibleMoves(Board board, PieceState myColor) {
+        ArrayList<Move> possibleMoves = new ArrayList<>();
+        for (char row = '7'; row >= '1'; row--) {
+            for (char col = 'a'; col <= 'g'; col++) {
+                int index = Board.index(col, row);
+                if (board.getContent(index) == myColor) {
+                    ArrayList<Move> addMoves
+                            = assistPossibleMoves(board, row, col);
+                    possibleMoves.addAll(addMoves);
+                }
+            }
+        }
+        return possibleMoves;
+    }
+
+    /** Returns an Arraylist of legal moves.
+     * @param board the board for testing
+     * @param row the row coordinate of the center
+     * @param col the col coordinate of the center */
+    private ArrayList<Move>
+        assistPossibleMoves(Board board, char row, char col) {
+        ArrayList<Move> assistPossibleMoves = new ArrayList<>();
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
+                if (i != 0 || j != 0) {
+                    char row2 = (char) (row + j);
+                    char col2 = (char) (col + i);
+                    Move currMove = Move.move(col, row, col2, row2);
+                    if (board.moveLegal(currMove)) {
+                        assistPossibleMoves.add(currMove);
+                    }
+                }
+            }
+        }
+        return assistPossibleMoves;
+    }
+}
